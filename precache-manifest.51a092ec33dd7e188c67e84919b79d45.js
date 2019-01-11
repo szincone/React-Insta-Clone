152f591d7846d12b44a024af25edbde38ed78be1
szincone <sawyerzincone@gmail.com>
2019-01-11 09:46:45 -0700

    Updates

---
+++ precache-manifest.51a092ec33dd7e188c67e84919b79d45.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Insta-Clone/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4ce5c51d8b5aa9a869a1",
+    "url": "/React-Insta-Clone/static/css/main.6b98a826.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-Insta-Clone/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "8aaddb3b8e893d54fa79",
+    "url": "/React-Insta-Clone/static/js/1.8aaddb3b.chunk.js"
+  },
+  {
+    "revision": "8bea3be130bc96ea5570",
+    "url": "/React-Insta-Clone/static/js/runtime~main.8bea3be1.js"
+  },
+  {
+    "revision": "aa19e81803124ddd2d6210ff89701c11",
+    "url": "/React-Insta-Clone/static/media/instagramLogoText.aa19e818.png"
+  },
+  {
+    "revision": "4ce5c51d8b5aa9a869a1",
+    "url": "/React-Insta-Clone/static/js/main.4ce5c51d.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Insta-Clone/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Insta-Clone/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Insta-Clone/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "8aaddb3b8e893d54fa79",
+    "url": "/React-Insta-Clone/static/css/1.2487da96.chunk.css"
+  },
+  {
+    "revision": "5d218fcc8e5202b7623ab9faa39f03d9",
+    "url": "/React-Insta-Clone/index.html"
+  }
+];
